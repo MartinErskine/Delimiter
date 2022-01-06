5fc79fac7da106e341ce2b66cdfe0a3000d94adc
Martin Erskine <martin.erskine@gmail.com>
2022-01-06 15:23:26 +0000

    Add Multiple delimiters.

--- Delimiter/Program.cs
+++ Delimiter/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Fclp;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Fclp.Internals.Extensions;
 
 namespace Delimiter
 {
@@ -34,6 +36,7 @@ namespace Delimiter
         private static int ParseDelimiter(string[] args)
         {
             var delimiter = ",";
+            var delimiters = new List<string>();
 
             if (args[0].StartsWith("//"))
             {
@@ -46,6 +49,10 @@ namespace Delimiter
                     delimiter = Regex.Matches(delimiterProvided[0], pattern).Cast<Match>()
                         .Select(m => m.Groups[1].Value)
                         .FirstOrDefault();
+
+                    delimiters = Regex.Matches(delimiterProvided[0], pattern).Cast<Match>()
+                        .Select(m => m.Groups[1].Value)
+                        .ToList();
                 }
                 else
                 {
@@ -54,12 +61,61 @@ namespace Delimiter
                 
                 var body = delimiterProvided[1];
 
-                return Parse(body, delimiter);
-                
+                return Parse(body, delimiters);
             }
 
             return Parse(args[0], delimiter);
         }
+
+        private static int Parse(string str, List<string> delimiterList)
+        {
+            if (str.Contains(@"\n"))
+            {
+                str = str.Replace(@"\n", delimiterList.FirstOrDefault());
+            }
+
+            var errorMessage = "Negative numbers were provided:\n";
+            var errors = false;
+            var delimiters = delimiterList.ToArray();
+            var total = 0;
+            var nums = str.Split(delimiters, StringSplitOptions.None);
+
+            foreach (var num in nums)
+            {
+                var ignoreMe = 0;
+                var isSuccessful = int.TryParse(num, out ignoreMe);
+                var numInt = int.Parse(num);
+
+                if (numInt < 0)
+                {
+                    errors = true;
+                    errorMessage += $"{numInt}\n";
+                }
+
+                if (numInt > 1000)
+                {
+                    numInt = 0;
+                }
+
+                if (!isSuccessful)
+                {
+                    return 0;
+                }
+
+                total += numInt;
+            }
+
+            if (!errors)
+            {
+                return total;
+            }
+
+            Console.WriteLine(errorMessage);
+            total = 0;
+
+            return total;
+        }
+
 
         private static int Parse(string str, string delimiter)
         {
