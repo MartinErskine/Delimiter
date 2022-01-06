94e540189b0490a69f1919c4cc0439dae83dc732
Martin Erskine <martin.erskine@gmail.com>
2022-01-06 13:22:31 +0000

    Add User Delimter

--- Delimiter/Program.cs
+++ Delimiter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Fclp;
 using System;
+using System.Linq;
 
 namespace Delimiter
 {
@@ -23,30 +24,55 @@ namespace Delimiter
 
             if (args.Length > 0)
             {
-                var replaceNewline = args[0].Replace(@"\n", ",");
+                var delimiter = ",";
 
-
-                var nums = replaceNewline.Split(',');
-
-
-                var total = 0;
-
-                foreach (var num in nums)
+                if (args[0].StartsWith("//"))
                 {
-                    var ignoreMe = 0;
+                    var delimiterProvided = args[0].Split(@"\n");
 
-                    var isSuccessful =  int.TryParse(num, out ignoreMe);
+                    delimiter = delimiterProvided[0].Substring(2);
+                    var body = delimiterProvided[1];
 
-                    if (!isSuccessful)
-                    {
-                        return 0;
-                    }
-
-                    total += int.Parse(num);
+                    return Parse(body, delimiter);
                 }
 
-                return total;
+                return Parse(args[0], delimiter);
+
             }
+
+            return 0;
+        }
+
+        private static int Parse(string str, string delimiter)
+        {
+            if (str.Contains("\n"))
+            {
+                str = str.Replace(@"\n", delimiter);
+            }
+
+
+            var nums = str.Split(delimiter);
+
+
+            var total = 0;
+
+            foreach (var num in nums)
+            {
+                var ignoreMe = 0;
+
+                var isSuccessful = int.TryParse(num, out ignoreMe);
+
+                if (!isSuccessful)
+                {
+                    return 0;
+                }
+
+                total += int.Parse(num);
+            }
+
+            return total;
+
+
 
             return 0;
         }
